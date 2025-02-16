f374075a574448505250861ba4d443b3d165fc71
Sachin Bisht <sachinbisht@Sachins-MacBook-Pro.local>
2025-02-17 01:20:19 +0530

    added theme to the  app

--- app/views.py
+++ app/views.py
@@ -1,9 +1,16 @@
-import flask
+import flask, random, time
 from app import app
 
 
 @app.route("/")
 def hello_world():
     return flask.render_template(
-        "home.html",
+        "home.html", cache_buster=random.random(), time=time
     )
+
+@app.after_request
+def add_cache_control(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Pragma"] = "no-cache"
+    response.headers["Expires"] = "0"
+    return response
