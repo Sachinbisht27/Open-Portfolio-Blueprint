ed0563b3e9e898854f88c9a5d704cdf5b776f7c0
Sachin Bisht <sachinbisht27@gmail.com>
2026-02-05 14:28:10 +0530

    Updated routing for the pages

--- app/views.py
+++ app/views.py
@@ -5,8 +5,28 @@ from app import app
 
 
 @app.route("/")
-def hello_world():
+def home():
     return flask.render_template("home.html", cache_buster=random.random(), time=time)
+
+
+@app.route("/projects")
+def projects():
+    return flask.render_template("projects.html", cache_buster=random.random(), time=time)
+
+
+@app.route("/projects/dost")
+def project_dost():
+    return flask.render_template("project_dost.html", cache_buster=random.random(), time=time)
+
+
+@app.route("/resume")
+def resume():
+    return flask.render_template("resume.html", cache_buster=random.random(), time=time)
+
+
+@app.route("/contact")
+def contact():
+    return flask.render_template("contact.html", cache_buster=random.random(), time=time)
 
 
 @app.after_request
